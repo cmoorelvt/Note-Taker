a2b45d6efff30cf86cd84a6f0b09bd2ea1a7c4aa
Christine Moore <cmoorelvt@gmail.com>
2020-11-01 18:18:36 -0500

    working out some bugs

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "routes")));
+app.use(express.static(path.join(__dirname)));
 
 require('./routes/routes.js')(app);
 
